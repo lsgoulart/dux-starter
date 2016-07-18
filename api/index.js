e26f3fabbae4a23d8ab112608877150419d51c9b
Leonardo Goulart <leonardo.sgoulart@gmail.com>
2016-07-17 23:42:07 -0300

    Adding some configs

---
+++ api/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('posts', function(req, res) {
+  res.json({
+    
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* eslint no-console: 0 */
+
 var express = require('express');
 var path = require('path');
 var cors = require('cors');
@@ -5,6 +7,8 @@ var webpack = require('webpack');
 var webpackMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config.dev');
+var api = require('./api');
+
 
 var isDeveloping = process.env.NODE_ENV !== 'production';
 var port = isDeveloping ? 3000 : process.env.PORT;
@@ -12,6 +16,7 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(__dirname + '/public'));
+app.use('/api', api);
 
 if(isDeveloping) {
   var compiler = webpack(config);
@@ -31,9 +36,7 @@ if(isDeveloping) {
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
   app.get('*', function(req, res) {
-    // res.sendFile(path.join(__dirname, 'index.html'));
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'index.html')));
-    res.end();
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 } else {
   app.use(express.static(__dirname, '/dist'));
@@ -43,9 +46,6 @@ if(isDeveloping) {
 }
 
 app.listen(port, '0.0.0.0', function onStart(err) {
-  if (err) {
-    return console.log(err);
-  }
-  
+  if (err) return console.log(err);
   console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
